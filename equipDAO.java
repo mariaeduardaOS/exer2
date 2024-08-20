1699c2145a8b5e0d690f37645df41abe83a48313
mariaeduardaOS <83980857+mariaeduardaOS@users.noreply.github.com>
2024-08-20 11:14:56 -0300

    Update equipDAO.java

--- equipDAO.java
+++ equipDAO.java
@@ -1,13 +1,13 @@
 import java.sql.*;
 
-public class EquipDAO {
+public class equipDAO {
     private Connection connection;
 
-    public EquipDAO() throws SQLException {
+    public equipDAO() throws SQLException {
         this.connection = ConexaoBD.getInstance().getConnection();
     }
 
-    public void inserirEquipamento(Equipamento equipamento) throws SQLException {
+    public void inserirEquipamento(equipDAO equipamento) throws SQLException {
         String sql = "INSERT INTO Equipamento (ID_Equipamento, Nome_Equipamento, Tipo) VALUES (?, ?, ?)";
         try (PreparedStatement stmt = connection.prepareStatement(sql)) {
             stmt.setInt(1, equipamento.getIdEquipamento());
@@ -15,5 +15,17 @@ public class EquipDAO {
             stmt.setString(3, equipamento.getTipo());
             stmt.executeUpdate();
         }
+    }
+
+    private int getIdEquipamento() {
+        throw new UnsupportedOperationException("Unimplemented method 'getIdEquipamento'");
+    }
+
+    private String getNomeEquipamento() {
+        throw new UnsupportedOperationException("Unimplemented method 'getNomeEquipamento'");
+    }
+
+    private String getTipo() {
+        throw new UnsupportedOperationException("Unimplemented method 'getTipo'");
     }
 }
