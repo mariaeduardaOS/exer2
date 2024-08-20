8d633ea3e1fba6a25a1d8dd4d00f0d4f30df7cd6
mariaeduardaOS <83980857+mariaeduardaOS@users.noreply.github.com>
2024-08-20 09:41:39 -0300

    Create projetoDAO.java

---
+++ projetoDAO.java
@@ -0,0 +1,57 @@
+import java.sql.Date;
+
+public class Projeto {
+    private int nomeProje;
+    private String nomeProje;
+    private String local;
+    private Date dtTermino;
+    private Date dtTermino;
+
+    public Projeto(int nomeProje, String nomeProje, String local, Date dtTermino, Date dtTermino) {
+        this.nomeProje = nomeProje;
+        this.nomeProje = nomeProje;
+        this.local = local;
+        this.dtTermino = dtTermino;
+        this.dtTermino = dtTermino;
+    }
+
+    public int getnomeProje() {
+        return nomeProje;
+    }
+
+    public String getnomeProje() {
+        return nomeProje;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public Date getdtTermino() {
+        return dtTermino;
+    }
+
+    public Date getdtTermino() {
+        return dtTermino;
+    }
+
+    public void setnomeProje(int nomeProje) {
+        this.nomeProje = nomeProje;
+    }
+
+    public void setnomeProje(String nomeProje) {
+        this.nomeProje = nomeProje;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public void setdtTermino(Date dtTermino) {
+        this.dtTermino = dtTermino;
+    }
+
+    public void setdtTermino(Date dtTermino) {
+        this.dtTermino = dtTermino;
+    }
+}
