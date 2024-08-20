c8047b76309fcb191284098f524f7ed2818d5a4b
mariaeduardaOS <83980857+mariaeduardaOS@users.noreply.github.com>
2024-08-20 09:38:15 -0300

    Update material.java

--- material.java
+++ material.java
@@ -1,1 +1,35 @@
+public class Material {
+    private int idMaterial;
+    private String nomeMaterial;
+    private int quantidade;
 
+    public Material(int idMaterial, String nomeMaterial, int quantidade) {
+        this.idMaterial = idMaterial;
+        this.nomeMaterial = nomeMaterial;
+        this.quantidade = quantidade;
+    }
+
+    public int getIdMaterial() {
+        return idMaterial;
+    }
+
+    public String getNomeMaterial() {
+        return nomeMaterial;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setIdMaterial(int idMaterial) {
+        this.idMaterial = idMaterial;
+    }
+
+    public void setNomeMaterial(String nomeMaterial) {
+        this.nomeMaterial = nomeMaterial;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+}
