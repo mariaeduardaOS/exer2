7a53b95e9e159e63923c1da3cb3790a56517f551
mariaeduardaOS <83980857+mariaeduardaOS@users.noreply.github.com>
2024-08-20 11:16:16 -0300

    Update projetoDAO.java

--- projetoDAO.java
+++ projetoDAO.java
@@ -1,22 +1,16 @@
 import java.sql.Date;
 
-public class Projeto {
-    private int nomeProje;
+public class projetoDAO {
     private String nomeProje;
     private String local;
     private Date dtTermino;
-    private Date dtTermino;
 
-    public Projeto(int nomeProje, String nomeProje, String local, Date dtTermino, Date dtTermino) {
+    public projetoDAO(String nomeProje, String local, Date dtTermino) {
         this.nomeProje = nomeProje;
         this.nomeProje = nomeProje;
         this.local = local;
         this.dtTermino = dtTermino;
         this.dtTermino = dtTermino;
-    }
-
-    public int getnomeProje() {
-        return nomeProje;
     }
 
     public String getnomeProje() {
@@ -31,14 +25,6 @@ public class Projeto {
         return dtTermino;
     }
 
-    public Date getdtTermino() {
-        return dtTermino;
-    }
-
-    public void setnomeProje(int nomeProje) {
-        this.nomeProje = nomeProje;
-    }
-
     public void setnomeProje(String nomeProje) {
         this.nomeProje = nomeProje;
     }
@@ -51,7 +37,4 @@ public class Projeto {
         this.dtTermino = dtTermino;
     }
 
-    public void setdtTermino(Date dtTermino) {
-        this.dtTermino = dtTermino;
-    }
 }
