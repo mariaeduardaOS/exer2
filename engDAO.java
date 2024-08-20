a0d1436c37142e7f43009bd9392cc7e7aaa3ddf4
mariaeduardaOS <83980857+mariaeduardaOS@users.noreply.github.com>
2024-08-20 11:14:13 -0300

    Update engDAO.java

--- engDAO.java
+++ engDAO.java
@@ -8,11 +8,11 @@ public class engDAO {
         this.connection = ConexaoBD.getInstance().getConnection();
     }
 
-    public void inserirEng(Eng eng) throws SQLException {
+    public void inserirEng(eng eng) throws SQLException {
         String sql = "INSERT INTO Engenheiro (ID_Eng, Nome_Eng, Especialidade) VALUES (?, ?, ?)";
         try (PreparedStatement stmt = connection.prepareStatement(sql)) {
-            stmt.setInt(1, eng.getIdEng());
-            stmt.setString(2, eng.getNomeEng());
+            stmt.setInt(1, eng.getidEng());
+            stmt.setString(2, eng.getnomeEng());
             stmt.setString(3, eng.getEspecialidade());
             stmt.executeUpdate();
         }
