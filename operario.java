dd4e8e0e97701292a3a4501144b6368eb0498d5b
mariaeduardaOS <83980857+mariaeduardaOS@users.noreply.github.com>
2024-08-20 09:35:52 -0300

    Create operario.java

---
+++ operario.java
@@ -0,0 +1,71 @@
+public class public class operario {
+    private int idoperario;
+    private String nomeoperario;
+    private String funcao;
+
+    public operario(int idoperario, String nomeoperario, String funcao) {
+        this.idoperario = idoperario;
+        this.nomeoperario = nomeoperario;
+        this.funcao = funcao;
+    }
+
+    public int getIdoperario() {
+        return idoperario;
+    }
+
+    public String getnomeoperario() {
+        return nomeoperario;
+    }
+
+    public String getFuncao() {
+        return funcao;
+    }
+
+    // Setters
+    public void setIdoperario(int idoperario) {
+        this.idoperario = idoperario;
+    }
+
+    public void setnomeoperario(String nomeoperario) {
+        this.nomeoperario = nomeoperario;
+    }
+
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
+} {
+    private int idoperario;
+    private String nomeoperario;
+    private String funcao;
+
+    public operario(int idoperario, String nomeoperario, String funcao) {
+        this.idoperario = idoperario;
+        this.nomeoperario = nomeoperario;
+        this.funcao = funcao;
+    }
+
+    public int getIdoperario() {
+        return idoperario;
+    }
+
+    public String getnomeoperario() {
+        return nomeoperario;
+    }
+
+    public String getFuncao() {
+        return funcao;
+    }
+
+    // Setters
+    public void setIdoperario(int idoperario) {
+        this.idoperario = idoperario;
+    }
+
+    public void setnomeoperario(String nomeoperario) {
+        this.nomeoperario = nomeoperario;
+    }
+
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
+}
