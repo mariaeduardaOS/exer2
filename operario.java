09697ae391c0749081927c4d3977638537faed1b
mariaeduardaOS <83980857+mariaeduardaOS@users.noreply.github.com>
2024-08-20 11:15:36 -0300

    Update operario.java

--- operario.java
+++ operario.java
@@ -1,4 +1,4 @@
-public class public class operario {
+public class operario {
     private int idoperario;
     private String nomeoperario;
     private String funcao;
@@ -21,42 +21,6 @@ public class public class operario {
         return funcao;
     }
 
-    // Setters
-    public void setIdoperario(int idoperario) {
-        this.idoperario = idoperario;
-    }
-
-    public void setnomeoperario(String nomeoperario) {
-        this.nomeoperario = nomeoperario;
-    }
-
-    public void setFuncao(String funcao) {
-        this.funcao = funcao;
-    }
-} {
-    private int idoperario;
-    private String nomeoperario;
-    private String funcao;
-
-    public operario(int idoperario, String nomeoperario, String funcao) {
-        this.idoperario = idoperario;
-        this.nomeoperario = nomeoperario;
-        this.funcao = funcao;
-    }
-
-    public int getIdoperario() {
-        return idoperario;
-    }
-
-    public String getnomeoperario() {
-        return nomeoperario;
-    }
-
-    public String getFuncao() {
-        return funcao;
-    }
-
-    // Setters
     public void setIdoperario(int idoperario) {
         this.idoperario = idoperario;
     }
